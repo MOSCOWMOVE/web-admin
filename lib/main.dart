ae18e6cfd441eddb8b5db1c693b1f3798afd792a
Firesieht <firesieht@mail.ru>
2021-10-13 22:53:00 +0300

    swap map to html and connect to flutter, make markers

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: ListViwMainMap(),
+        body: MapWidget(),
       ),
     );
   }
