764e7dc1cf3f9f1c60d38c51fe5d3d8513935a8f
Firesieht <firesieht@mail.ru>
2021-10-15 00:30:50 +0300

    add dots

--- lib/components/assets/src/index.ts
+++ lib/components/assets/src/index.ts
@@ -1,4 +1,5 @@
 import * as mapboxgl from "mapbox-gl";
+import { LibManifestPlugin } from "webpack";
 import img from "./location_pin.png";
 
 (mapboxgl as any).accessToken = 'pk.eyJ1IjoiZmlyZXNpZWh0IiwiYSI6ImNrdW9kemYzbTB4ZGkycHAxbXN2YnIzaGMifQ.G0fl-qVbecucfOvn8OtU4Q';
@@ -10,17 +11,34 @@ center: [37.61, 55.7 ], // starting position [lng, lat]
 zoom: 9 // starting zoom
 });
 
-let dots = { 
-    markers: [
-            [37.65, 55.88],
-            [37.55, 55.75],
-            [37.80, 55.69],
-        ]
+
+
+
+
+export const getCircleCords = (radius:number, steps:number, center:number[]) =>{
+    let cords = []
+    for (let  i = 1; i <= steps; i++) {
+        let x0 = center[0]
+        let y0 = center[1] 
+        cords.push([
+            (x0 + 1.8 * radius * Math.cos(2 * Math.PI * i / steps)),
+            (y0  + radius * Math.sin(2 * Math.PI * i / steps))
+        ])
     }
-                
-localStorage.dots = JSON.stringify(dots)
+    return cords
+}
+
 
 let markers:any = []
+let markers500: number[][][] = []
+let markers1000: number[][][] = []
+let markers3000: number[][][] = []
+let markers5000: number[][][] = []
+let outlines = new Array()
+
+JSON.parse(localStorage.dots).markers.forEach((element:any) => {
+    console.log(element)
+})
 
 JSON.parse(localStorage.dots).markers.forEach((dot:any) => {
     markers.push(
@@ -29,14 +47,62 @@ JSON.parse(localStorage.dots).markers.forEach((dot:any) => {
                         'type': 'Feature',
                         'geometry': {
                             'type': 'Point',
-                            'coordinates': dot
+                            'coordinates': dot.cords
                         },
                         'properties': {
                             'title': 'name'
                         }
                     },
     )
+
+    if(dot.type == 500){
+        let cords = getCircleCords(0.0044990383305613, 50 , dot.cords)
+        markers500.push(cords)
+        outlines.push({
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "LineString",
+                "coordinates": cords
+            }
+        })
+    }
+    if(dot.type == 1000){
+        let cords = getCircleCords(0.008998076661122683, 50 ,  dot.cords)
+        markers1000.push(cords)
+        outlines.push({
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "LineString",
+                "coordinates": cords
+            }
+        })    }
+    if(dot.type == 3000){
+        let cords = getCircleCords(0.026994229983368048, 50 ,  dot.cords)
+        markers3000.push(cords)
+        outlines.push({
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "LineString",
+                "coordinates": cords
+            }
+        })    }
+    if(dot.type == 5000){
+        let cords = getCircleCords(0.04499038330561341, 50 ,  dot.cords)
+        markers5000.push(cords)
+        outlines.push({
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "LineString",
+                "coordinates": cords
+            }
+        })
+    }
 });
+
 
 
 const geo500mjson = {
@@ -45,32 +111,138 @@ const geo500mjson = {
         {
         'type': 'Feature',
         'geometry': {
-                'type': 'Point',
-                'coordinates': [37.65, 55.88]
+                'type': 'Polygon',
+                'coordinates': markers500
+            }
+        }
+    ]
+};
+
+const geo1000mjson = {
+    'type': 'FeatureCollection',
+    'features': [
+        {
+        'type': 'Feature',
+        'geometry': {
+                'type': 'Polygon',
+                'coordinates': markers1000
+            }
+        }
+    ]
+};
+
+const geo3000mjson = {
+    'type': 'FeatureCollection',
+    'features': [
+        {
+        'type': 'Feature',
+        'geometry': {
+                'type': 'Polygon',
+                'coordinates': markers3000
+            }
+        }
+    ]
+};
+
+const geo5000mjson = {
+    'type': 'FeatureCollection',
+    'features': [
+        {
+        'type': 'Feature',
+        'geometry': {
+                'type': 'Polygon',
+                'coordinates': markers5000
             }
         }
     ]
 };
 
 map.on('load', () => {
-
-map.addSource('point', {
+    map.addSource('points500m', {
         'type': 'geojson',
         'data': (geo500mjson as any)
-        });
-        map.addLayer(
+    });
+    map.addSource('points1000m', {
+        'type': 'geojson',
+        'data': (geo1000mjson as any)
+    });
+    map.addSource('points3000m', {
+        'type': 'geojson',
+        'data': (geo3000mjson as any)
+    });
+    map.addSource('points5000m', {
+        'type': 'geojson',
+        'data': (geo5000mjson as any)
+    });
+
+
+    map.addLayer(
             {
                 'id': 'r500',
-                'type': 'circle',
-                'source': 'point',
+                'type': 'fill',
+                'source': 'points500m',
+                'layout': {},
                 'paint': {
-                'circle-radius': 15,
-                'circle-color': '#F84C4C' // red color
+                    'fill-color': '#FF002E',
+                    'fill-opacity': 0.25
                 }
             }
         )
+    map.addLayer(
+            {
+                'id': 'r1000',
+                'type': 'fill',
+                'source': 'points1000m',
+                'layout': {},
+                'paint': {
+                    'fill-color': '#FD5C01',
+                    'fill-opacity': 0.25
+                }
+            }
+        )
+    map.addLayer(
+            {
+                'id': 'r3000',
+                'type': 'fill',
+                'source': 'points3000m',
+                'layout': {},
+                'paint': {
+                    'fill-color': '#F9C200',
+                    'fill-opacity': 0.25
+                }
+            }
+        )
+    map.addLayer(
+            {
+                'id': 'r5000',
+                'type': 'fill',
+                'source': 'points5000m',
+                'layout': {},
+                'paint': {
+                    'fill-color': '#46FF90',
+                    'fill-opacity': 0.25
+                }
+            }
+    )
 
 
+    map.addLayer({
+            "id": "route",
+            "type": "line",
+            "source": {
+                "type": "geojson",
+                "data": {
+                    
+                        'type': 'FeatureCollection',
+                        'features': outlines
+                    }
+            },
+            "paint": {
+                "line-color": "#888",
+                "line-width": 3,
+                "line-dasharray": [4, 4]
+            }
+        });
 
 map.loadImage(
     img,
--- lib/components/map.dart
+++ lib/components/map.dart
@@ -1,17 +1,59 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import 'dart:html' as html;
 import 'dart:js' as js;
 import 'dart:ui' as ui;
-
+import 'dart:convert';
+import 'package:localstorage/localstorage.dart';
 
 
 
 class MapWidget extends StatelessWidget {
-  const MapWidget({  Key key }) : super(key: key);  
+  MapWidget({  Key key }) : super(key: key);  
+  
+  
+  LocalStorage storage = new LocalStorage('localstorage_app');
+
+
+  var dots = { 
+    "markers" : [
+        {
+
+            "id" : 1001,
+            "cords" : [37.65, 55.88],
+            "type" : 500,
+            "amount" : 2
+        },
+        {
+            "id" : 1002,
+            "cords": [37.55, 55.75],
+            "type": 1000,
+            "amount": 5
+        },
+        {
+            "id" : 1003,
+            "cords":  [37.80, 55.69],
+            "type": 3000,
+            "amount": 7
+        },
+        {
+            "id" : 1004,
+            "cords":  [37.47, 55.86],
+            "type": 5000,
+            "amount": 10
+        }
+
+        ]
+    };
+  
+
   
   @override
   Widget build(BuildContext context) {
+    storage.setItem('dots', jsonEncode(dots));
+
     // ignore: undefined_prefixed_name
     ui.platformViewRegistry.registerViewFactory(
         'map',
